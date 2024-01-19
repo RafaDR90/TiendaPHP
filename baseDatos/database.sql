21ca0937dbe6af4b831697bfcde837bd973d8826
Rafael Delgado Ramirez <122291110+RafaDR90@users.noreply.github.com>
2024-01-19 19:30:35 +0100

    Update database.sql

--- baseDatos/database.sql
+++ baseDatos/database.sql
@@ -56,9 +56,9 @@ DROP TABLE IF EXISTS pedidos;
 CREATE TABLE IF NOT EXISTS pedidos(
                                       id              int(255) auto_increment not null,
                                       usuario_id      int(255) not null,
-    /* provincia       varchar(100) not null,
+     provincia       varchar(100) not null,
      localidad       varchar(100) not null,
-     direccion       varchar(255) not null,*/
+     direccion       varchar(255) not null,
                                       coste           float(200,2) not null,
                                       estado          varchar(20) not null,
                                       fecha           date,
